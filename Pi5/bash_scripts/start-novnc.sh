0070ce2bce8ce2361bddd4c68c4dce4ae872ae43
Alanj818 <145750400+Alanj818@users.noreply.github.com>
2026-05-27 16:32:51 -0700

    another change to the bash script

--- Pi5/bash_scripts/start-novnc.sh
+++ Pi5/bash_scripts/start-novnc.sh
@@ -3,10 +3,13 @@
 export DISPLAY=:1
 
 Xvfb :1 -screen 0 1280x800x24 &
+sleep 2
 
 startxfce4 &
+sleep 3
 
 x11vnc -display :1 -nopw -forever -shared -rfbport 5900 &
+sleep 2
 
 /usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 8080 &
 
