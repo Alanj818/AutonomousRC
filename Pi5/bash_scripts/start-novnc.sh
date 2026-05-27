6e563c5b893a3db1e37e65a1baa0fb26dd593e19
Alanj818 <145750400+Alanj818@users.noreply.github.com>
2026-05-27 16:10:43 -0700

    changed the script

--- Pi5/bash_scripts/start-novnc.sh
+++ Pi5/bash_scripts/start-novnc.sh
@@ -8,6 +8,6 @@ startxfce4 &
 
 x11vnc -display :1 -nopw -forever -shared -rfbport 5900 &
 
-novnc_proxy --vnc localhost:5900 --listen 8080 &
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 8080 &
 
 sleep infinity
